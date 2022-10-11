5229a2e7fdf88c7c403179ddd54aa9f03a8cfd3f
arzumkucuk <arzummkucuk@gmail.com>
2022-10-11 12:54:04 -1000

    day4-selenium

---
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P0_findElementPractice1.java
@@ -0,0 +1,43 @@
+package com.cybertek.tests.day4_findElements_checkboxes;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class P0_findElementPractice1 {
+    public static void main(String[] args) {
+
+        //#1- Open chrome browser
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        //#2 - go to: http://practice.cybertekschool.com/forgot_password
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        //#3- Locate all of the LINKS on the page, and print out all the texts
+              // first we need to find the locator that is returning us all of the links in the page
+                // body//a
+              // second, store all of the links in to a list of WebElements
+
+        List<WebElement> listOfLinks = driver.findElements(By.xpath("//body//a"));
+
+              // third, create java logic to print out all of the texts of all links
+
+        for (WebElement eachlink : listOfLinks){
+
+            System.out.println(eachlink.getText());
+        }
+
+        //#4- Print out how many links on the page
+
+
+
+
+    }
+}
--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P1_Xpath_CssSelector.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P1_Xpath_CssSelector.java
@@ -28,7 +28,7 @@ public class P1_Xpath_CssSelector {
         //WebElement homeLink = driver.findElement(By.xpath("//a[@href='/']"));
 
         // b. "Forgot password" header
-        WebElement forgotPasswordHeader = driver.findElement(By.xpath(""));
+        WebElement forgotPasswordHeader = driver.findElement(By.xpath("//h2[.='Forgot Password']"));
 
         // c. "E-mail" text
         WebElement emaillabel = driver.findElement(By.xpath("//label[@for='email']"));
@@ -39,10 +39,34 @@ public class P1_Xpath_CssSelector {
 
 
         // d. E-mail input box
+        //Using syntax #1, tagName[attribute='value']
         WebElement emailInputBox = driver.findElement(By.cssSelector("input[type='text']"));
-        // e. "retieve password" button
+
+        //option #2: Using "name" attribute to locate the same web element
+       // WebElement emailInputbox = driver.findElement(By.cssSelector("input[name='email']"));
+
+        // e. "retrieve password" button
+        // Locating the button using #2 cssSelector syntax.
+        // tagName.classValue
+        // tagName#idValue
+
+        // locating using class  value
+        WebElement retrievePasswordbutton = driver.findElement(By.cssSelector("button.radius"));
+
+
         // f. "Powered by Cybertek School" text
+        WebElement footerText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+
         // 4. Verify all WebElements are displayed.
+
+        if (homeLink.isDisplayed() && forgotPasswordHeader.isDisplayed() && emaillabel.isDisplayed()
+        && emailInputBox.isDisplayed() && retrievePasswordbutton.isDisplayed() && footerText.isDisplayed() ){
+            System.out.println("ALL WEB ELEMENTS ARE DISPLAYED. VERIFICATION PASSED!");
+        }else {
+            System.out.println("One or more of the web elements are not displayed. Verification FAILED!!!");
+        }
+
+        driver.close();
 
 
 
