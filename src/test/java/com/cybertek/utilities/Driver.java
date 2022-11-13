66d1905a0c57960b6bfc1efbf84e12d449cc1f94
arzumkucuk <arzummkucuk@gmail.com>
2022-11-13 10:52:31 -1000

    day9-selenium

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -42,4 +42,11 @@ public class Driver {
         return driver;
     }
 
+    public static void closeDriver(){
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+
 }
