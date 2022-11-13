2a7dce3af02c96ca4dd01282b3251e09541174e1
arzumkucuk <arzummkucuk@gmail.com>
2022-11-13 10:50:01 -1000

    day9-selenium

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -3,6 +3,9 @@ package com.cybertek.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -18,14 +21,25 @@ public class Driver {
             String browser = ConfigurationReader.getProperty("browser");
 
             switch (browser) {
+
                 case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     break;
             }
         }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        return null;
+        return driver;
     }
 
 }
